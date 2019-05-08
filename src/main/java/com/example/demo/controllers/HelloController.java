be95d4f654d02ec43ae3ce45d9ee68bb4591de89
mid1528 <vp80462@globalymc.com>
2019-05-08 22:12:37 +0800

    2019-05-08 huang 添加excel的导入导出方法

--- src/main/java/com/example/demo/controllers/HelloController.java
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -1,15 +1,32 @@
 package com.example.demo.controllers;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.time.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.beans.FileUtil;
+import com.example.demo.beans.Person;
+import com.example.demo.service.UserManageService;
+
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
+	@Autowired
+	UserManageService userManageService;
+	@Autowired
+	FileUtil fileUtil;
+	
     @GetMapping
     public String getWelcomeMsg() {
     	
@@ -30,4 +47,34 @@ public class HelloController {
     public String helloUser() {       
         return "Hello,user";
     }
+    
+    @RequestMapping("export")
+    public void export(HttpServletResponse response){
+
+        //模拟从数据库获取需要导出的数据
+        List<Person> personList = new ArrayList<>();
+        Person person1 = new Person("路飞","1",new Date());
+        Person person2 = new Person("娜美","2", new Date());
+        Person person3 = new Person("索隆","1", new Date());
+        Person person4 = new Person("小狸猫","1", new Date());
+        personList.add(person1);
+        personList.add(person2);
+        personList.add(person3);
+        personList.add(person4);
+
+        //导出操作
+        FileUtil.exportExcel(personList,"花名册","草帽一伙",Person.class,"海贼王.xls",response);
+    }
+    
+    @RequestMapping("importExcel")
+    public void importExcel(){
+        String filePath = "F:\\海贼王.xls";
+        //解析excel，
+        List<Person> personList = fileUtil.importExcel(filePath,1,1,Person.class);
+        //也可以使用MultipartFile,使用 FileUtil.importExcel(MultipartFile file, Integer titleRows, Integer headerRows, Class<T> pojoClass)导入
+        System.out.println("导入数据一共【"+personList.size()+"】行");
+
+        //TODO 保存数据库
+    }
+
 }
